18967ea367ef58d6b763641bf6671d34072ac416
NomiPeaceMaker <60261713+NomiPeaceMaker@users.noreply.github.com>
2023-02-15 02:04:21 +1030

    Added volunteering work

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {volunteer, education, experience, SectionId, skills} from '../../../data/data';
+import {education, experience, SectionId, skills, volunteer} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
 import {SkillGroup} from './Skills';
